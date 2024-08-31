c7e7c0cac35c4553201beafecb0d422efe902d37
shayin <shayinbanu29@gmail.com>
2024-08-31 11:43:38 +0530

    Added Index.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {path:'header',component:HeaderComponent,title:'Header'},
+ {path:'',redirectTo:'header', pathMatch :'full'}
+];
